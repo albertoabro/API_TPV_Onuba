cae77b6b102658f510e1c3a5474eb1ac87d5fde5
Alberto <albertoabro@gmail.com>
2023-04-05 14:07:04 +0200

    Revert "add login"
    This reverts commit 684f9569e1c4234e495790f2369f38097c27d348.

--- app.js
+++
@@ -1,9 +0,0 @@
-const app = require('./adpaters/express/server');
-
-const PORT = process.env.PORT || 3000;
-
-const server = app.listen(PORT, ()=>{
-    console.log(`Server is running on port ${PORT}`);
-})
-
-module.exports = {app, server};
--- domain/family.js
+++
@@ -1,8 +0,0 @@
-class Family{
-    constructor({idFamily, nameFamily}){
-        this.idFamily=idFamily;
-        this.nameFamily=nameFamily;
-    }
-}
-
-module.exports = Family;
